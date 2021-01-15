5987429b46abdaf2f6e4473300d25f7fad6f02b8
hocci <hocci32@gmail>
2021-01-16 04:22:14 +0800

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Narbar from './components/Navbar'
+import Particles from "react-particles-js";
+import Narbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Narbar />    
+    <>
+      <Particles
+        className="particles-canvas" 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area:900, 
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "random",
+              }
+            },
+            links: {              
+              color: "random",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,            
+            },
+          }
+        }}
+      />
+      <Narbar />
+      <Header />
+    </>    
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
             <div className="container">
                 <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={faBars} style={{color:"white"}}/> 
+                    <FontAwesomeIcon icon={faBars} style={{color:"#fff"}}/> 
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
